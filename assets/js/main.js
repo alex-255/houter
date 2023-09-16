43c5ca4ac2f73be972378a96b81a2ad4ee158dbd
alex-255 <okozlov7@gmail.com>
2023-09-16 19:04:17 +0200

    video-tour section 1 part

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
+( function( $ ) {
+
+
+
 window.addEventListener("load", () => {
 
     const header = document.getElementById("top-part");
@@ -13,9 +17,92 @@ window.addEventListener("load", () => {
   };
 
 
+  document.querySelectorAll(".our-rec--top-bar--menu--item").forEach(function (item) {
+    
+      item.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        if (item.classList.contains("house")) {
+          document.querySelectorAll(".our-rec--slider--item.house").forEach((element)=>{
+            element.classList.add("shown");
+          });
+          document.querySelectorAll(".our-rec--slider--item:not(.house)").forEach((element)=>{
+            element.classList.remove("shown");
+          });
+
+          item.classList.add("shown");
+          document.querySelectorAll(".our-rec--top-bar--menu--item:not(.house)").forEach((element)=>{
+            element.classList.remove("shown");
+          });
+        } 
+        
+        if (item.classList.contains("villa")) {
+          document.querySelectorAll(".our-rec--slider--item.villa").forEach((element)=>{
+            element.classList.add("shown");
+          });
+          document.querySelectorAll(".our-rec--slider--item:not(.villa)").forEach((element)=>{
+            element.classList.remove("shown");
+          });
+
+          item.classList.add("shown");
+          document.querySelectorAll(".our-rec--top-bar--menu--item:not(.villa)").forEach((element)=>{
+            element.classList.remove("shown");
+          });
+        } 
+        
+        if (item.classList.contains("apartment")) {
+          document.querySelectorAll(".our-rec--slider--item.apartment").forEach((element)=>{
+            element.classList.add("shown");
+          });
+          document.querySelectorAll(".our-rec--slider--item:not(.apartment)").forEach((element)=>{
+            element.classList.remove("shown");
+          });
+
+          item.classList.add("shown");
+          document.querySelectorAll(".our-rec--top-bar--menu--item:not(.apartment)").forEach((element)=>{
+            element.classList.remove("shown");
+          });
+
+          $('.our-rec--slider').slick('unslick');
+          $('.our-rec--slider').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: true
+          });
+
+        }
+
+        if (item.classList.contains("all")) {
+          document.querySelectorAll(".our-rec--slider--item").forEach((element)=>{
+            element.classList.add("shown");
+          });
+
+          item.classList.add("shown");
+          document.querySelectorAll(".our-rec--top-bar--menu--item:not(.all)").forEach((element)=>{
+            element.classList.remove("shown");
+          });
+        }
+      });
+    
+
+    
+
+  });
+  
+  
+
 });
 
-( function( $ ) {
+
+
+
+
+
+
+
+
     
     $(document).ready(function(){
       $('.menu-area--slider').slick({
@@ -23,7 +110,7 @@ window.addEventListener("load", () => {
           slidesToShow: 2,
           slidesToScroll: 1,
           dots: false,
-          arrows: false,
+          arrows: true,
           autoplay: true,
           // adaptiveHeight: true,
           responsive: [
@@ -52,18 +139,23 @@ window.addEventListener("load", () => {
         dots: false,
         arrows: true,
         responsive: [
-          
+          {
+            breakpoint: 1350,
+            settings: {
+              arrows: false
+            }
+          },
           {
             breakpoint: 1200,
             settings: {
-              slidesToShow: 2
+              slidesToShow: 2,
+              arrows: false
             }
           },
             {
               breakpoint: 900,
               settings: {
                 slidesToShow: 1,
-                dots: false,
                 arrows: false
               }
             }
